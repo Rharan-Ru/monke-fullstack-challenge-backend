9e5c7595db81f81d6e232e3c3259f094cf43d476
Rharan-Ru <rharanru@gmail.com>
2024-10-28 06:21:04 -0300

    feat: Add decorators and filters for handling public routes and exceptions

---
+++ src/shared/filters/http-exception.filter.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+
+import {
+  ExceptionFilter,
+  Catch,
+  ArgumentsHost,
+  HttpException,
+} from '@nestjs/common';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const status = exception.getStatus();
+    const message = exception.getResponse();
+    console.error({
+      status,
+      message,
+      timestamp: new Date().toISOString(),
+    });
+    response.status(status).json({
+      statusCode: status,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+      message,
+    });
+  }
+}
